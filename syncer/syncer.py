2e7e707d54ec7d0ee53ced9f6613484be06fcc43
Derongan <dgfried@gmail.com>
2020-04-05 23:49:32 -0700

    Update syncer to read from repos file.

--- syncer/syncer.py
+++ syncer/syncer.py
@@ -1,19 +1,22 @@
+import argparse
+import csv
 import zipfile
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 import requests
 
+
 def get_plugin(org: str, repo: str, artifact_name: str, output_path: Path, workflow_name: str = None,
-               workflow_id: str = None):
+               workflow_id: str = None, auth: tuple = None):
     repo_endpoint = f'https://api.github.com/repos/{org}/{repo}'
 
     if workflow_id is None:
-        workflows = requests.get(f"{repo_endpoint}/actions/workflows").json()
+        workflows = requests.get(f"{repo_endpoint}/actions/workflows", auth=auth).json()
 
         workflow_id = next(x for x in workflows["workflows"] if x['name'] == workflow_name)["id"]
 
-    runs = requests.get(f"{repo_endpoint}/actions/workflows/{workflow_id}/runs",
+    runs = requests.get(f"{repo_endpoint}/actions/workflows/{workflow_id}/runs", auth=auth,
                         params={"branch": "master", "status": "success"}).json()
 
     if runs['total_count'] >= 0:
@@ -21,24 +24,45 @@ def get_plugin(org: str, repo: str, artifact_name: str, output_path: Path, workf
 
         artifacts_url = latest_run['artifacts_url']
 
-        artifacts = requests.get(artifacts_url).json()['artifacts']
+        artifacts = requests.get(artifacts_url, auth=auth).json()['artifacts']
 
         archive_download_url = next(x for x in artifacts if x['name'] == artifact_name)['archive_download_url']
 
         with NamedTemporaryFile(delete=True) as f:
-            r = requests.get(archive_download_url, auth=("derongan", "<>"),
+            r = requests.get(archive_download_url, auth=auth,
                              allow_redirects=True)
 
             f.write(r.content)
 
             with zipfile.ZipFile(f) as fz:
-                fz.extractall(output_path)
+                if len(fz.namelist()) != 1:
+                    print("Artifact should have only one jar")
+                    return
+                zipinfo = fz.getinfo(fz.namelist()[0])
+
+                zipinfo.filename = f"{repo}.jar"
+
+                fz.extract(zipinfo, output_path)
+
+
+def process_line(values: list, output_dir: Path, auth: tuple):
+    org_name, repo_name, artifact_name, workflow_name = values
+
+    print(f"Fetching plugin {repo_name}...")
+    get_plugin(org_name, repo_name, artifact_name, output_dir, workflow_name=workflow_name, auth=auth)
 
 
 if __name__ == "__main__":
-    get_plugin("MineInAbyss", "StaminaClimb", "package", Path.cwd()/'../plugins', workflow_name="Java CI")
-    get_plugin("MineInAbyss", "DangerousWaters", "package", Path.cwd()/'../plugins', workflow_name="Java CI")
-    get_plugin("MineInAbyss", "MineInAbyss", "package", Path.cwd()/'../plugins', workflow_name="Java CI")
-    get_plugin("MineInAbyss", "DeeperWorld", "package", Path.cwd()/'../plugins', workflow_name="Java CI")
-    get_plugin("MineInAbyss", "geary", "package", Path.cwd()/'../plugins', workflow_name="Java CI")
+    parser = argparse.ArgumentParser(description='Download Plugin Jars')
+    parser.add_argument('-u', '--username', type=str, help='github username', required=True)
+    parser.add_argument('-t', '--token', type=str, help='github token', required=True)
+    parser.add_argument('file', type=argparse.FileType('r'), help='path to repo file')
+    parser.add_argument('output_dir', type=Path, help='output directory')
 
+    args = parser.parse_args()
+
+    auth = (args.username, args.token)
+
+    repo_reader = csv.reader(args.file, skipinitialspace=True)
+    for row in repo_reader:
+        process_line(row, args.output_dir, auth)
